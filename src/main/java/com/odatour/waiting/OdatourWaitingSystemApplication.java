4ba88839e7cd7be07db3c783794e59a6261fc539
sungjun <opr99@naver.com>
2026-05-06 22:48:37 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/odatour/waiting/OdatourWaitingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.odatour.waiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdatourWaitingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdatourWaitingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odatour/waiting/OdatourWaitingSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.odatour.waiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdatourWaitingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
